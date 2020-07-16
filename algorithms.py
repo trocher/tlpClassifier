bbfb08904e62a3d4c1d9f53db46595c4607488a4
trocher <rocher14@live.fr>
2020-07-16 21:34:49 +0200

    improving RE lb and ub

--- algorithms.py
+++ algorithms.py
@@ -105,7 +105,7 @@ def round_eliminator_lb(problem, iterations, labels):
     if not result_b and not result_w:
         return -1
         
-    def get_value(result,start):
+    def get_value(result):
         for i in reversed(range(1000)):
             search_string_i  = 'Lower bound of ' + str(i)
             if result.find(search_string_i) != -1:
